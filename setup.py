1ca4f8dff65727a5e543ce19537816a0901b1194
tintinweb <tintinweb@oststrom.com>
2018-09-26 21:50:20 +0200

    prepare v0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version="0.1.2"
+version="0.1.3"
 
 setup(
     name="ethereum-dasm",
